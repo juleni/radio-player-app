4ce254dc6fa60622a68ce0b62d9dd35dcd650924
juleni <69912179+juleni@users.noreply.github.com>
2022-11-24 19:11:46 +0100

    Added texts and Radio Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Radio from "./Radio";
 
 function App() {
-  return <div className="App">Radio App</div>;
+  return (
+    <div className="App">
+      <h1>Radio Player App</h1>
+      <h2>Choose genre, station and start listening</h2>
+      <Radio />
+    </div>
+  );
 }
 
 export default App;
