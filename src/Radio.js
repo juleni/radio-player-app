0b938a09a8e1b652d38740de8b267a310d488679
juleni <69912179+juleni@users.noreply.github.com>
2022-11-24 19:12:00 +0100

    Implemented Radio component

---
+++ src/Radio.js
@@ -0,0 +1,90 @@
+import { RadioBrowserApi } from "radio-browser-api";
+import React, { useEffect, useState } from "react";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import defaultImage from "./default-radio-station.jpg";
+
+export default function Radio() {
+  const [stations, setStations] = useState();
+  const [stationFilter, setStationFilter] = useState("pop");
+
+  useEffect(() => {
+    setupApi(stationFilter).then((data) => {
+      setStations(data);
+    });
+  }, [stationFilter]);
+
+  const setupApi = async (stationFilter) => {
+    const api = new RadioBrowserApi(fetch.bind(window), "My Radio App");
+    const stations = await api.searchStations({
+      language: "english",
+      tag: stationFilter,
+      limit: 30,
+    });
+
+    return stations;
+  };
+
+  const setDefaultSrc = (event) => {
+    event.target.src = defaultImage;
+  };
+
+  const filters = [
+    "all",
+    "classical",
+    "country",
+    "dance",
+    "disco",
+    "house",
+    "jazz",
+    "pop",
+    "rap",
+    "retro",
+    "rock",
+  ];
+
+  return (
+    <div className="radio">
+      <div className="filters">
+        {filters.map((filter, index) => {
+          return (
+            <span
+              key={index}
+              className={stationFilter === filter ? "selected" : ""}
+              onClick={() => setStationFilter(filter)}
+            >
+              {filter}
+            </span>
+          );
+        })}
+      </div>
+      <div className="stations">
+        {stations &&
+          stations.map((station, index) => {
+            return (
+              <div className="station" key={index}>
+                <div className="stationName">
+                  <img
+                    className="logo"
+                    src={station.favicon}
+                    alt="Station logo"
+                    onError={setDefaultSrc}
+                  />
+                  <div className="name">{station.name}</div>
+                </div>
+                <AudioPlayer
+                  className="player"
+                  src={station.urlResolved}
+                  showJumpControls={false}
+                  layout="stacked"
+                  customProgressBarSection={[]}
+                  customControlsSection={["MAIN_CONTROLS", "VOLUME_CONTROLS"]}
+                  autoPlayAfterSrcChange={false}
+                />
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+}
