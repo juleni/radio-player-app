56247749c89016295353f26c69c9164086f4c39b
juleni <69912179+juleni@users.noreply.github.com>
2023-06-29 17:33:15 +0200

    Added Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import "./Footer.css";
+export default function Footer() {
+  return (
+    <div className="footer radio">
+      <div className="juleni">
+        <a
+          href="https://juleni.github.io/portfolio"
+          target="noopener noreferrer"
+        >
+          JULENI 2023
+        </a>
+      </div>
+    </div>
+  );
+}
